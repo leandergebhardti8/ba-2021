0ce303bc1af1b5640ac315cc19bd964dbf3a0c7c
Leander Gebhardt <l.gebhardt@integr8.com>
2021-06-04 13:17:04 +0200

    some important dependencies

--- App/ba-project/src/App.vue
+++ App/ba-project/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png">
   </div>
 </template>
 
@@ -22,7 +22,9 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+}
+body {
+  background: linear-gradient(90deg, #1CB5E0 0%, #000851 100%);
 }
 </style>
