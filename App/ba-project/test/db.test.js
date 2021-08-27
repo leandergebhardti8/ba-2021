a1a6683bc74df6ec79f1c2f2a398170d0c8843a5
Leander Gebhardt <l.gebhardt@integr8.com>
2021-08-27 11:31:07 +0200

    first tests

---
+++ App/ba-project/test/db.test.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+describe("Database Tests", function() {
+    describe('Connection to DB with mongoURI', function(){
+        it('Connection to DB sucessfull', function(done){
+            mongoose.connect(keys.mongoURI, {
+                useNewUrlParser: true, 
+                useUnifiedTopology: true, 
+                useCreateIndex: true
+            }, done);
+        });
+    })
+    describe("Connection to DB with wrong mongoURI", function() {
+        it('Connection to DB should fail', function(done){
+            try{
+                let connection = mongoose.connect('loclahost:3000', {
+                    useNewUrlParser: true, 
+                    useUnifiedTopology: true, 
+                    useCreateIndex: true
+                });
+                expect(connection).to.throw(Error);
+            }
+            catch (error) {
+                done()
+            }
+        })
+    })
+})
