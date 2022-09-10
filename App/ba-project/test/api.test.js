cc6f66852714d84438cf0e285ee66f6b6b598097
Leander Gebhardt <Leander.Gebhardt@Student.HTW-Berlin.de>
2022-09-10 19:58:48 +0200

    Update api.test.js

--- App/ba-project/test/api.test.js
+++ App/ba-project/test/api.test.js
@@ -6,7 +6,7 @@ const expect = require("chai").expect;
 describe("API Tests", function() {
     describe("GET projects", function () {
         it("returns all projects", async function () {
-            const response = await request.get("projects")
+            const response = await request.get("project")
 
             expect(response.status).to.eql(200)
             expect(response).to.not.be.empty;
@@ -32,30 +32,4 @@ describe("API Tests", function() {
             expect(response).to.not.be.empty;
         })
     })
-
-    // describe("GET project", function () {
-    //     it("returns test project created ", async function () {
-    //         const response = await request.get(`project/${projectId}`)
-
-    //         expect(response.status).to.eql(200)
-    //         expect(response).to.not.be.empty;
-    //     })
-    // })
-
-    // describe("UPDATE project", function () {
-    //     it("able to update a project", async function () {
-    //         const savedProjects = await request.get("projects")
-    //         let projectToUpdate = {}
-    //         for(let i=0; i < savedProjects.length; i++) {
-    //             if(savedProjects[i].name === "Project for automated testing")
-    //             projectToUpdate = savedProjects[i]
-    //         }
-    //         const response = await request
-    //             .put(`project/${projectToUpdate._id}`)
-    //             .send({name: "Changed Projekt for automated testing"})
-
-    //         expect(response.status).to.eql(200)
-    //         done()
-    //     })
-    // })
 })
